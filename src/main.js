f76220b66796245563235703e75ed980300db1f9
Nicole Tsingas <nicolecass27@gmail.com>
2023-05-04 12:19:56 -0300

    Feito a logica de pesquisar os pokemons atraves do nome ou tipo, pela função filterData

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
- import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import {filterData} from './data.js'; //importando função
+
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-console.log(example, data);
-console.log(data);
 
 let currentUser;
-let nameUser = document.querySelector("#name");
+const nameUser = document.querySelector("#name");
 
 const homePage = document.querySelector('#home-nav');
 const pokemonsPage = document.querySelector('#pokemons-nav');
@@ -16,47 +13,60 @@ const divHome = document.querySelector('.home');
 const divPokemons = document.querySelector('.pokemons');
 const divAnime = document.querySelector('.anime');
 const divPokemonGo = document.querySelector('.pokemongo');
-
 const pokemons = document.querySelector('.div-parent-pokemon');
 const pokemonList = data.pokemon;
+const inputSearch = document.querySelector("#search-input");
+const statisticText = document.querySelector(".statistic-data");
+
+//listeners
+inputSearch.addEventListener("input", () => {
+  const filteredPokemonList = filterData(pokemonList, inputSearch.value)
+  renderPokemon(filteredPokemonList);
+});
 
 window.addEventListener("pageshow", function(){
-    currentUser = localStorage.getItem("nomeUsuario");
-    nameUser.innerText = currentUser;
+  currentUser = localStorage.getItem("nomeUsuario");
+  nameUser.innerText = currentUser;
 });
 
 homePage.addEventListener('click', function(){
-    showHideDiv('.home')
+  showHideDiv('.home')
 }) ;
 
 pokemonsPage.addEventListener('click', function(){
-    showHideDiv('.pokemons')
+  showHideDiv('.pokemons')
 });
 
 animePage.addEventListener('click', function(){
-    showHideDiv('.anime')
+  showHideDiv('.anime')
 });
 
 pokemonGoPage.addEventListener('click', function(){
-    showHideDiv('.pokemongo')
+  showHideDiv('.pokemongo')
 });
 
-function showHideDiv(pokemon){
-    const content = document.querySelector(pokemon)
-    divHome.classList.remove('visible')
-    divPokemons.classList.remove('visible')
-    divAnime.classList.remove('visible')
-    divPokemonGo.classList.remove('visible')
-    content.classList.add('visible')
-};
+//functions/for
 
- for (let i=0; i < pokemonList.length; i++){
+function showHideDiv(pokemon){
+  const content = document.querySelector(pokemon)
+  divHome.classList.remove('visible')
+  divPokemons.classList.remove('visible')
+  divAnime.classList.remove('visible')
+  divPokemonGo.classList.remove('visible')
+  content.classList.add('visible')
+}
+
+function renderPokemon(pokemonList){
+  pokemons.innerHTML = "";
+  for (let i=0; i < pokemonList.length; i++){
     const card = document.createElement("div")
     card.classList.add("card-pokemon")
     card.innerHTML = `
-        <img src="${pokemonList[i].img}" alt="imagem-pokemon">
-        <p>Nome: ${pokemonList[i].name}</p>
-        <p>Elemento: ${pokemonList[i].type}</p>
-        `
+    <img src="${pokemonList[i].img}" alt="imagem-pokemon">
+    <p class="pokemon-name"><strong>Nome:</strong> ${pokemonList[i].name}</p>
+    <p class="pokemon-type"><strong>Elemento:</strong> ${pokemonList[i].type}</p>
+    `
     pokemons.appendChild(card) // colocar o card dentro da div (div-parent-pokemon)
+  }
 }
+renderPokemon(pokemonList);
